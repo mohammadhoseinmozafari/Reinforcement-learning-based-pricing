f326fecf4f87928d25eea87ffcb79a04f91d24b6
mohammadhoseinmozafari <mohammad.hosein.mozafaryk@gmail.com>
2026-06-13 15:19:07 -0400

    added: bbp support to metrics, added: regime tracking to metrics

--- bbp/train/metrics.py
+++ bbp/train/metrics.py
@@ -13,9 +13,14 @@ class TrainingMetrics:
     episode_opp_profits: List[float] = field(default_factory=list)
 
     episode_prices: List[float] = field(default_factory=list)
-    episode_opp_prices: List[float] = field(default_factory=list)
+    episode_opp_uniform_prices: List[float] = field(default_factory=list)
+    episode_opp_new_prices: List[float] = field(default_factory=list)
+    episode_opp_old_prices: List[float] = field(default_factory=list)
 
     episode_market_shares: List[float] = field(default_factory=list)
+    episode_regimes : List[float] = field(default_factory=list)
+    episode_opp_regimes: List[float] =field(default_factory=list)
+
     eval_rewards: List[float] = field(default_factory=list)
     critic_losses: List[float] = field(default_factory=list)
     actor_losses: List[float] = field(default_factory=list)
@@ -25,10 +30,15 @@ class TrainingMetrics:
     step_profits: List[float] = field(default_factory=list)
     step_prices: List[float] = field(default_factory=list)
     step_market_shares: List[float] = field(default_factory=list)
+    step_regimes : List[float] = field(default_factory=list)
     
     step_opp_profits: List[float] = field(default_factory=list)
-    step_opp_prices: List[float] = field(default_factory=list)
-    
+    step_opp_uniform_prices: List[float] = field(default_factory=list)
+    step_opp_new_prices: List[float] = field(default_factory=list)
+    step_opp_old_prices: List[float] = field(default_factory=list)
+
+    step_opp_regimes : List[float] = field(default_factory=list)
+
     def reset_episode(self):
         """Reset per-episode tracking."""
         self.step_profits = []
@@ -36,8 +46,11 @@ class TrainingMetrics:
         self.step_market_shares = []
 
         self.step_opp_profits=[]
-        self.step_opp_prices=[]
     
+        self.step_opp_uniform_prices = []
+        self.step_opp_new_prices = []
+        self.step_opp_old_prices = []
+
     def record_step(self, info: Dict):
         """Record metrics from a step."""
         self.step_profits.append(info.get("profit", 0.0))
@@ -45,8 +58,10 @@ class TrainingMetrics:
         self.step_market_shares.append(info.get("market_share", 0.0))
     
         self.step_opp_profits.append(info.get("opponent_profit", 0.0))
-        self.step_opp_prices.append(info.get("opponent_price", 0.0))
 
+        self.step_opp_uniform_prices.append(info.get("opponent_price_uniform", 0.0))
+        self.step_opp_new_prices.append(info.get("opponent_price_new", 0.0))
+        self.step_opp_new_prices.append(info.get("opponent_price_old", 0.0))
 
     def end_episode(self, total_reward: float):
         """Finalize episode metrics."""
@@ -54,8 +69,11 @@ class TrainingMetrics:
         self.episode_profits.append(sum(self.step_profits))
         self.episode_prices.append(float(np.mean(self.step_prices)) if self.step_prices else 0.0)
         self.episode_market_shares.append(float(np.mean(self.step_market_shares)) if self.step_market_shares else 0.0)
+        self.episode_opp_regimes.append(float(np.mean(self.step_opp_regimes)))
+        self.episode_regimes.append(float(np.mean(self.step_regimes)))
 
         self.episode_opp_profits.append(sum(self.step_opp_profits))
         
-        self.episode_opp_prices.append(float(np.mean(self.step_opp_prices)) if self.step_opp_prices else 0.0)
-        
+        self.episode_opp_uniform_prices.append(float(np.mean(self.step_opp_uniform_prices)) if self.step_opp_uniform_prices else 0.0)
+        self.episode_opp_new_prices.append(float(np.mean(self.step_opp_new_prices)) if self.step_opp_new_prices else 0.0)
+        self.episode_opp_old_prices.append(float(np.mean(self.step_opp_old_prices)) if self.step_opp_old_prices else 0.0)
