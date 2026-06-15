9102a60d8172277954588e34cfb95005eb4febf3
mohammadhoseinmozafari <mohammad.hosein.mozafaryk@gmail.com>
2026-06-15 19:38:28 -0400

    nothing new

--- bbp/opt_main.py
+++ bbp/opt_main.py
@@ -1,7 +1,7 @@
 from optimization.uniform_pricing import grid_search
 
-grid_search('aggressive_uniform', 0.05)
-grid_search('passive_aggressive_uniform', 0.05)
-grid_search('passive_uniform', 0.05)
-grid_search('premium_passive_uniform', 0.05)
-grid_search('premium_uniform', 0.05)
+grid_search('aggressive_bbp', 0.01)
+grid_search('passive_aggressive_bbp', 0.01)
+grid_search('passive_bbp', 0.01)
+grid_search('premium_passive_bbp', 0.01)
+grid_search('premium_bbp', 0.01)
