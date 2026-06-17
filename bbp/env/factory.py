8ccecdc0dd3200d7bc0526febedf912c05f94130
mohammadhoseinmozafari <mohammad.hosein.mozafaryk@gmail.com>
2026-06-17 17:14:15 -0400

    added : evaluation run for uniform vs bbp

--- bbp/env/factory.py
+++ bbp/env/factory.py
@@ -2,32 +2,39 @@
 
 
 from enum import Enum
-from typing import Callable
+from typing import Callable, Optional
 
 from env import make_uniform_pricing_env
+from models.reward_normalizer import FixedRewardNormalizer
 
 
 
 class EnvironmentType (str, Enum):
     UNIFORM_PRICING = 'uniform_pricing'
+    BBP_PRICING = 'bbp_pricing'
 
 class EnvironmentFactory :
     
-    def __init__(self, env_type : EnvironmentType , reward_normalizer : type) -> None:
+    def __init__(self, env_type : EnvironmentType , reward_normalizer : type = FixedRewardNormalizer) -> None:
         self._env_type = env_type
         self.env_creator : Callable = self.resolve_env_creator (self._env_type)
         self.reward_normalizer = reward_normalizer
 
 
-    def create_environment (self,  opponent_type : str , config  ):
+    def create_environment (self,  opponent_type : str , config  ) :
+        
         base_env = self.env_creator(
-            opponent = opponent_type,
-            num_consumers = config.num_consumers,
-            episode_length = config.episode_length,
-            seed = config.seed
-        )
+                opponent = opponent_type,
+                num_consumers = config.num_consumers,
+                episode_length = config.episode_length,
+                seed = config.random_seed
+            )
+        
+
+            
         env = self.reward_normalizer(base_env)
         return base_env, env
+
 
 
 
---
+++ bbp/uniform_vs_bbp_eval.py
@@ -0,0 +1,45 @@
+from env.factory import EnvironmentFactory, EnvironmentType
+import os
+import json
+
+from evaluation.config import EvaluationConfig
+from evaluation.evaluator import Evaluator
+from models.SAC import SAC
+from models.buffer import ReplayBuffer
+from train.uniform_training.curriculum import BBPOpponentUniformPricingCurriculum
+
+MODEL_PATH = "experiments/uniform_pricing/bbp_opp/runs/1/sac_uniform_final.pt"
+SAVE_PATH = "experiments/uniform_pricing/bbp_opp/eval/"
+opponent_types = BBPOpponentUniformPricingCurriculum().opponent_types
+
+os.makedirs(SAVE_PATH, exist_ok=True)
+
+config = EvaluationConfig(
+    env_type= EnvironmentType.UNIFORM_PRICING,
+    model_path= MODEL_PATH
+)
+env_factory = EnvironmentFactory(
+    config.env_type
+)
+
+evaluator = Evaluator(config)
+agent = SAC(
+    state_dim=9, 
+    action_dim=1,
+    hidden_dim=32,
+    replay_buffer=ReplayBuffer(100)
+)
+
+agent.load(config.model_path)
+
+for opp_type in opponent_types :
+    _, env = env_factory.create_environment(opp_type, config)
+
+    eval_result = evaluator.evaluate(agent, env).to_dict()
+
+    result_save_path = os.path.join(SAVE_PATH, f"eval_result_{opp_type}.json")
+    with open(result_save_path, 'w') as f:
+        json.dump(eval_result, f, indent=2)
+
+    
+
