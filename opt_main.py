f9f4c41e83deaf67f376bfdd5dbb85c30d65756c
mohammadhoseinmozafari <mohammad.hosein.mozafaryk@gmail.com>
2026-07-01 08:45:52 -0400

    removed : redundant files

--- opt_main.py
+++
@@ -1,7 +0,0 @@
-from optimization.uniform_pricing import grid_search
-
-grid_search('aggressive_bbp', 0.01)
-grid_search('passive_aggressive_bbp', 0.01)
-grid_search('passive_bbp', 0.01)
-grid_search('premium_passive_bbp', 0.01)
-grid_search('premium_bbp', 0.01)
